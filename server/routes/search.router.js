2522f8b34093a528f5a65c8d10c9999d23f05589
Lee Xiong <tadbitz@yahoo.com>
2023-04-06 11:22:37 -0500

    search router get completed

---
+++ server/routes/search.router.js
@@ -0,0 +1,20 @@
+const { default: axios } = require('axios');
+const express = require('express');
+const pool = require('../modules/pool');
+require('dotenv').config();
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    console.log('my .env:', process.env)
+
+    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}`)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
