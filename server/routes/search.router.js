d36c8595110b218008a0e364d7902e2e4e5c54a5
Anthony Cole <anthony.cole83@yahoo.com>
2023-04-07 11:34:35 -0500

    we got our images to show on the DOM.

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -6,16 +6,18 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 
+//:taco is place holder for 'searchTerm'.  
+  //taco is a property for req.params
+    //you could take taco off req.params but then you would need to put taco at the end of 'serchTerm' on line 16.  
+router.get('/:taco', (req, res) => {
+  let searchTerm = req.params.taco
+    console.log('this is my searchTerm', searchTerm)
 
-router.get('/:q', (req, res) => {
-  let image = req.params
-    console.log('my .env:', image)
-
-    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${image}`)
+    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${searchTerm}`)
   
     .then((response) => {
       res.send(response.data);
-      console.log('lookng at response.data',response.data)
+      // console.log('lookng at response.data',response.data)
     })
     .catch((error) => {
       console.log('error in this code', error)
